a19cae0743628de88da330f2497c55bd4fc0227c
yokozawa <y.yokozawa@gmail.com>
2013-08-29 16:37:03 +0900

    add migrate

---
+++ db/migrate/20130829073442_board.rb
@@ -0,0 +1,13 @@
+class Board < ActiveRecord::Migration
+  def up
+    create_table :user_to_board do |t|
+      t.integer  :user_id
+      t.integer  :board_id
+      t.datetime :created_at
+      t.timestamp :updated_at
+    end
+  end
+
+  def down
+  end
+end
