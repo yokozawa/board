1ccff36354587aa5ec257502f945230ab6b11f8b
yokozawa <y.yokozawa@gmail.com>
2013-10-04 11:56:30 +0900

    change

--- public/js/ajax.js
+++ public/js/ajax.js
@@ -44,7 +44,7 @@ $(function(){
         $("<li>").attr({'data-id':resobj.id, id:resobj.id}).append(
           $("<a>").attr({href:'#', id:resobj.id, class:'ui-state-default', 'data-pk': resobj.id, 'data-name':'task', 'data-type':'text', 'data-url':'/edit', 'data-title':'enter'}).text(resobj.body)
           ,$("<span>").attr({class:'ui-icon ui-icon-trash deleteCmd'}).text('')
-          ,$('<span>').attr({class:'ui-icon ui-icon-arrowthick-2-n-s ui-corner-all ui-state-hover'}).text(' ')
+          ,$('<span>').attr({class:'ui-icon ui-icon-arrowthick-2-n-s ui-corner-all ui-state-hover moveCmd'}).text(' ')
         )
       );
     });
