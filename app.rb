1c17cac26a09d13bb0b579996aae326926230294
yokozawa <y.yokozawa@gmail.com>
2013-12-08 12:32:48 +0900

    --

--- app.rb
+++ app.rb
@@ -36,6 +36,7 @@ class User < ActiveRecord::Base
     user = self.where(email: email).first
     sha1_password = Digest::SHA1.hexdigest("#{salt}#{password}")
     pp BCrypt::Password.create(sha1_password).to_s
+    pp BCrypt::Password.new(sha1_password)
     pp user.password_hash
     nil
     # if BCrypt::Password.new(sha1_password) == user.password_hash
