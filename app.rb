7a087ad5ab0637762368dcab40691a8f5dd38693
yokozawa <y.yokozawa@gmail.com>
2013-12-08 17:16:41 +0900

    --

--- app.rb
+++ app.rb
@@ -30,7 +30,6 @@ class User < ActiveRecord::Base
   has_many:boards, :through => :user_to_boards
 #  attr_accessible:board_ids
 
-
   def self.authenticate(email, password)
     user = self.where(email: email).first
     if self.encrypt_password(password) == user.password_hash
