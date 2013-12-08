35acc0a0f07d7a16aa822b3488e81e05078d2b29
yokozawa <y.yokozawa@gmail.com>
2013-12-08 12:24:20 +0900

    --

--- app.rb
+++ app.rb
@@ -37,6 +37,7 @@ class User < ActiveRecord::Base
     sha1_password = Digest::SHA1.hexdigest("#{salt}#{password}")
     pp sha1_password
     pp user.password_hash
+    nil
     # if BCrypt::Password.new(sha1_password) == user.password_hash
     #   user
     # else
