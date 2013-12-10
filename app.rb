b43a1b0cf37b6ab56690b4147637697c58b05a97
yokozawa <y.yokozawa@gmail.com>
2013-12-10 22:09:04 +0900

    --

--- app.rb
+++ app.rb
@@ -10,47 +10,10 @@ require 'digest/md5'
 ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 ActiveRecord::Base.establish_connection('development')
 
-# class Task < ActiveRecord::Base
-# end
-
-# class UserToBoard < ActiveRecord::Base
-#   belongs_to:board
-#   belongs_to:user
-# end
-
-# class Board < ActiveRecord::Base
-#   has_many:tasks
-#   has_many:user_to_boards
-#   has_many:users, :through => :user_to_boards
-# #  attr_accessible:user_ids
-# end
-
 ( Dir.glob("app/model/*.rb") ).each do |model|
   pp "./"+model
   require "./"+model
 end
-
-# class User < ActiveRecord::Base
-#   has_many:user_to_boards
-#   has_many:boards, :through => :user_to_boards
-# #  attr_accessible:board_ids
-
-#   def self.authenticate(email, password)
-#     user = self.where(email: email).first
-#     if self.encrypt_password(password) == user.password_hash
-#       return user
-#     else
-#       return nil
-#     end
-#   end
-
-#   def self.encrypt_password(password)
-#     salt = 'gerwg2$#H$"HRHAherwiahr$'
-#     if password.present?
-#       return Digest::MD5.hexdigest(password + salt)
-#     end
-#   end
-# end
 
 class MyApp < Sinatra::Base
 
