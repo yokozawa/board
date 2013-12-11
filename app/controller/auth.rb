7f9b2973c764565dc10a3ef219ab65211c869597
yokozawa <y.yokozawa@gmail.com>
2013-12-11 21:51:20 +0900

    --

---
+++ app/controller/auth.rb
@@ -0,0 +1,28 @@
+
+class Auth < Sinatra::Base
+  get '/log_out' do
+    session[:user_id] = nil
+    unless session[:user_id]
+      redirect 'log_in'
+    end
+  end
+
+  get '/log_in' do
+    erb :log_in
+  end
+
+  post '/log_in' do
+    if session[:user_id]
+      redirect "/"
+    end
+
+    user = User.authenticate(params[:email], params[:password])
+
+    if user
+     session[:user_id] = user.id
+     redirect '/'
+    else
+     redirect "/log_in"
+    end
+  end
+end
