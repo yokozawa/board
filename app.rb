44e2cf67c530d549f5dca0fd09912a0a6e123631
yokozawa <y.yokozawa@gmail.com>
2013-12-11 22:55:19 +0900

    --

--- app.rb
+++ app.rb
@@ -102,6 +102,32 @@ pp views
     end
   end
 
+  get '/log_out' do
+    session[:user_id] = nil
+    unless session[:user_id]
+      redirect 'log_in'
+    end
+  end
+
+  get '/log_in' do
+    erb :log_in
+  end
+
+  post '/log_in' do
+    if session[:user_id]
+      redirect "/"
+    end
+
+    user = User.authenticate(params[:email], params[:password])
+
+    if user
+     session[:user_id] = user.id
+     redirect '/'
+    else
+     redirect "/log_in"
+    end
+  end
+
   get '/request_token' do
     callback_url = "#{base_url}/access_token"
     Koala::Facebook::OAuth.new(ENV['APP_ID'], ['APP_SECRET'], callback_url)
